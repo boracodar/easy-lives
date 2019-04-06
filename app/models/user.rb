165471a04cdc07f941e059f430bc4563b5275602
Lucas Caton <lucascaton@gmail.com>
2019-04-06 10:14:35 +1000

    Create User

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+    :validatable, :confirmable
+
+  validates :name, presence: true
+end
