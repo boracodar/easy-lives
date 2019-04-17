860b2a122a0562b60285b082c87b7ddf589a7a8e
Lucas Caton <lucascaton@gmail.com>
2019-04-17 18:32:55 +1000

    Refactor notifications JS code

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-    $notification = $delete.parentNode;
-    $delete.addEventListener('click', () => {
-      $notification.parentNode.removeChild($notification);
-    });
-  });
+  setupNotificationCloseButton();
 });
+
+function setupNotificationCloseButton() {
+  document.querySelectorAll('.notification .delete').forEach((closeButton) => {
+    notification = closeButton.parentNode;
+
+    closeButton.addEventListener('click', () => notification.parentNode.removeChild(notification));
+  });
+}
