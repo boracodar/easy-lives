49ce42b4f6acfa978f5b07871dc6741a64e943cc
Tiago Garcia <tiago.tiago22@gmail.com>
2019-06-11 01:02:59 -0300

    Fix Rubocop 'Layout/AlignArguments' cop

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-    :validatable, :confirmable
+         :validatable, :confirmable
 
   validates :name, presence: true
 
