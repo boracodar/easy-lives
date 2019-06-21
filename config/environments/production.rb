b76145ec7e779dfa4520bd20311b4919ed586ef3
Lucas Caton <lucascaton@gmail.com>
2019-06-22 08:04:40 +1000

    Improve Rubocop configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -64,13 +64,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'easy-lives.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.net',
-    port:                 587,
-    authentication:       :plain,
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
     enable_starttls_auto: true,
-    domain:               'easy-lives.herokuapp.com',
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD']
+    domain: 'easy-lives.herokuapp.com',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD']
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
